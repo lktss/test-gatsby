491f9e205a6ca57990533a91ebfb9641184a387a
Lktss <machut.lucas@gmail.com>
2021-10-03 13:45:40 +0200

    Feat : ajout titre index.js
    ajout h1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,42 @@
 import * as React from "react"
- import { Link } from "gatsby"
+import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import {Row, Col, Container} from 'react-bootstrap';
-
+import { Row, Col, Container } from "react-bootstrap"
 
 const IndexPage = () => {
- return (
-  <Layout>
+  return (
+    <Layout>
       <SEO title="Accueil" />
-        <Row className="align-items-center h-100">
-            <Col xs={12} md={6} className="questions">
-                <Container data-aos="fade-right" className="text-center">
-                     <StaticImage
-                        src="../images/picture1.png"
-                        width={500}
-                        quality={100}
-                        
-                        alt="Quiz picture"
-                        style={{ margin: `auto` }}
-                      />
-                </Container>
-            </Col>
-            <Col  xs={12} md={6} className="reponses">
-                <Container data-aos="fade-left" className="p-5">
-                    <Link to="/rgpd"  className="link">
-                        <h3>Es-tu RGPD friendly ?</h3>
-                    </Link>
-                   <Link to="/quiz"  className="link">
-                       <h3>Quel Ubi'dreamer' es-tu ?</h3>
-                       </Link>
-                </Container>
-            </Col>
-        </Row>
+      <Row className="align-items-center h-100">
+        <Col xs={12} md={6} className="questions">
+          <Container data-aos="fade-right" className="text-center">
+            <StaticImage
+              src="../images/picture1.png"
+              width={500}
+              quality={100}
+              alt="Quiz picture"
+              style={{ margin: `auto` }}
+            />
+          </Container>
+        </Col>
+        <Col xs={12} md={6} className="reponses">
+          <Container data-aos="fade-left" className="p-5">
+            <Link to="/rgpd" className="link">
+              <h3>Es-tu RGPD friendly ?</h3>
+              <h1> CEci est un test</h1>
+            </Link>
+            <Link to="/quiz" className="link">
+              <h3>Quel Ubi'dreamer' es-tu ?</h3>
+            </Link>
+          </Container>
+        </Col>
+      </Row>
     </Layout>
- )
+  )
 }
 
 export default IndexPage
-
-
